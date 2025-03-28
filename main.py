a83c437ee8671469c3f097ccc60de842152148c8
waldir2004 <waldiraltamar2004@gmail.com>
2025-03-28 00:21:39 -0500

    eliminacion de controladores antiguos

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ from routes.roles_router import router as roles_router
 from routes.parametros_valor_router import router as parametros_valor_router
 from routes.parametros_router import router as parametros_router
 from routes.modulos_router import router as modulos_router
-from routes.historial_usuario_router import router as historial_router
-from routes.enfermedades_router import router as enfermedades_router
-from routes.categorias_enfermedades_router import router as categorias_enfermedades_router
 from routes.auth_routes import router as auth_router
 from routes.permisos_router import router as permisos_router
-from routes.prediccion_routers import router as prediccion_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -32,12 +28,8 @@ app.include_router(roles_router)
 app.include_router(parametros_valor_router)
 app.include_router(parametros_router)
 app.include_router(modulos_router)
-app.include_router(historial_router)
-app.include_router(enfermedades_router)
-app.include_router(categorias_enfermedades_router)
 app.include_router(auth_router)
 app.include_router(permisos_router)
-app.include_router(prediccion_router)
 
 if __name__ == "__main__":
     import uvicorn
